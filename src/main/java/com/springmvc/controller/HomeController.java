6b08e8e827f3658e0ec26c03b5fb9687029b1c77
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-11-28 13:26:14 +0100

    Build Contoller and view

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("This is home url");
+        model.addAttribute("name", "Jay Patel");
+        model.addAttribute("id",123);
         return "index";
+    }
+
+    @RequestMapping("/help")
+    public String help()
+    {
+        return "help";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -4,5 +4,12 @@
 </head>
 <body>
 <h2>This is home page...</h2>
+
+<%
+    String name = (String) request.getAttribute("name");
+    Integer id = (Integer) request.getAttribute("id");
+%>
+<h1>Name is: <%=name%></h1>
+<h1>Id is: <%=id%></h1>
 </body>
 </html>
